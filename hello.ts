1a59063dadd4441edc14b2c39f4aaa84965a486e
kazakami <kazakami@kmc.gr.jp>
2018-05-30 00:25:28 +0900

    画面描画

--- hello.ts
+++ hello.ts
@@ -1,6 +1,16 @@
-import { WebGLRenderer } from "three";
+import { WebGLRenderer, Scene, Camera, PerspectiveCamera } from "three";
 let renderer: WebGLRenderer = new WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
 let canvas: HTMLCanvasElement = renderer.domElement;
+let scene: Scene = new Scene();
+let camera: Camera = new PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 document.body.appendChild(canvas);
 
-console.log("hello");
+
+
+
+let animate = () => {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+animate();
